cabb92bb4046a8adf6cb5ba055d74a0b7855963a
StasON <stason1412@gmail.com>
2024-06-15 02:00:18 +0700

    sum

--- products/models.py
+++ products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='children')
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+    image = models.ImageField(upload_to='products_images/')
+    category = models.ForeignKey(ProductCategory, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
