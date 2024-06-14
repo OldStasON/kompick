e83540744737f832a4dc516622027c204743a4cb
StasON <stason1412@gmail.com>
2024-06-15 04:19:22 +0700

    new_base

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Brand, ProductCategory, Advertisement
 
 # Register your models here.
+admin.site.register(Brand)
+admin.site.register(ProductCategory)
+admin.site.register(Advertisement)
--- products/models.py
+++ products/models.py
@@ -1,22 +1,49 @@
 from django.db import models
 
 
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
 class ProductCategory(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='children')
+    brand = models.ForeignKey(Brand, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        # Если категория является дочерней, устанавливаем её бренд равным бренду родительской категории
+        if self.parent and not self.brand:
+            self.brand = self.parent.brand
+        super().save(*args, **kwargs)
 
-class Product(models.Model):
-    name = models.CharField(max_length=100)
+    def propagate_brand_to_children(self):
+        # Обновляем бренды всех дочерних категорий
+        for child in self.children.all():
+            child.brand = self.brand
+            child.save()
+            child.propagate_brand_to_children()  # Рекурсивно обновляем дочерние категории
+
+    def save_with_brand_propagation(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.propagate_brand_to_children()
+
+
+class Advertisement(models.Model):
+    title = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.PositiveIntegerField(default=1)
-    image = models.ImageField(upload_to='products_images/')
+    image = models.ImageField(upload_to='advertisements_images/')
     category = models.ForeignKey(ProductCategory, on_delete=models.PROTECT)
+    brand = models.ForeignKey(Brand, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
-        return self.name
+        return self.title
